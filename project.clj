ba71fd3940c3097921775cc2aacca8088aecfb46
Drew Raines <aaraines@gmail.com>
2012-10-02 13:56:45 -0500

    Bump version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.draines/monocle "0.9.16"
+(defproject com.draines/monocle "0.9.17-SNAPSHOT"
   :description "Monocle"
   :url "http://github.com/drewr/monocle"
   :license {:name "Eclipse Public License"
