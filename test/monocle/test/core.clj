ab0da62c5843396df05e8235d673c4bad606b585
Drew Raines <aaraines@gmail.com>
2012-10-09 12:36:08 -0500

    Clean up tests and handle bugfix in last commit.

--- test/monocle/test/core.clj
+++ test/monocle/test/core.clj
@@ -1,16 +1,17 @@
 (ns monocle.test.core
   (:use clojure.test)
   (:use monocle.core :reload-all)
-  (:require [clojure.java.io :as io])
+  (:require [clojure.java.io :as jio]
+            [monocle.io :as io])
   (:import (java.io File)))
 
-(def tmp nil)
+(def ofile nil)
 
 (use-fixtures :each
   (fn [t]
-    (with-redefs [tmp (File/createTempFile "__monocle-" ".offset")]
+    (with-redefs [ofile (File/createTempFile "__monocle-" ".offset")]
       (t)
-      (.delete tmp))))
+      (.delete ofile))))
 
 (deftest args
   (testing "testing opts"
@@ -24,44 +25,56 @@
            (seq (amqp-opts "amqp://localhost/test|test.exchange|foo"))))))
 
 (deftest offset
-  (testing "should publish offset after reading"
+  (testing "should NOT publish offset after reading"
     (let [out (with-out-str
-                (main {:file (.getPath (io/resource "log.txt"))
-                       :offset-file (str tmp)
+                (main {:file (.getPath (jio/resource "log.txt"))
+                       :offset-file ofile
                        :interface "stdout"
                        :batch 3
                        :reset true}))]
+      (is (= 0 (io/slurp-offset ofile)))
       (is (.startsWith out "{\"time"))
       (is (= 3 (count (re-seq #"-- \n" out))))))
   (testing "should publish offset after reading"
     (let [out (with-out-str
-                (main {:file (.getPath (io/resource "log.txt"))
-                       :offset-file (str tmp)
+                (main {:file (.getPath (jio/resource "log.txt"))
+                       :offset-file ofile
+                       :interface "stdout"
+                       :batch 3
+                       :reset false}))]
+      (is (= (.length (jio/file (jio/resource "log.txt")))
+             (io/slurp-offset ofile)))
+      (is (.startsWith out "{\"time"))
+      (is (= 3 (count (re-seq #"-- \n" out))))))
+  (testing "should not do anything since last read was to the end of file"
+    (let [out (with-out-str
+                (main {:file (.getPath (jio/resource "log.txt"))
+                       :offset-file ofile
+                       :interface "stdout"
+                       :batch 3
+                       :reset false}))]
+      (is (empty? out))))
+  (testing "should reset offset when it extends past file length"
+    (io/spit-offset ofile (inc (.length (jio/file (jio/resource "log.txt")))))
+    (let [out (with-out-str
+                (main {:file (.getPath (jio/resource "log.txt"))
+                       :offset-file ofile
                        :interface "stdout"
                        :batch 3
                        :reset false}))]
       (is (.startsWith out "{\"time"))
-      (is (= 3 (count (re-seq #"-- \n" out))))))
-  (testing "should publish offset after reading"
-    (let [out (with-out-str
-                (main {:file (.getPath (io/resource "log.txt"))
-                       :offset-file (str tmp)
-                       :interface "stdout"
-                       :batch 3
-                       :reset false}))]
-      (is (not (.startsWith out "{\"time")))
-      (is (= 0 (count (re-seq #"-- \n" out)))))))
+      (is (= 3 (count (re-seq #"-- \n" out)))))))
 
 (deftest bulk
   (testing "should add bulk format"
     (let [out (with-out-str
-                (main {:file (.getPath (io/resource "bulk.txt"))
-                       :offset-file (str tmp)
+                (main {:file (.getPath (jio/resource "bulk.txt"))
+                       :offset-file ofile
                        :interface "stdout"
                        :batch 1
                        :reset false
                        :index "foo"
                        :type "t"}))
-          ans (slurp (io/resource "bulk-answer.txt"))]
+          ans (slurp (jio/resource "bulk-answer.txt"))]
       (is (= out ans))
       (is (= 2 (count (re-seq #"-- \n" out)))))))
